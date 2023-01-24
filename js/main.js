606ce6621329b3f475cfbe43f71a6829cf7e2c14
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-01-24 10:58:27 -0600

    added stuff

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@ var accessToken = MAPBOX_API_KEY;
 mapboxgl.accessToken = accessToken;
 
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/outdoors-v12',
-    zoom: 1,
-    center: [-98.4916, 29.4252]
+    container: 'map', style: 'mapbox://styles/mapbox/outdoors-v12', zoom: 1, center: [-98.4916, 29.4252]
 });
 
 let markers = [];
@@ -23,6 +20,7 @@ function doSearch(event) {
 }
 
 function pinThatAddress(address) {
+    var date1 = new Date(0);
     geocode(address, accessToken)
         .then(function (result) {
             const marker = new mapboxgl.Marker({"color": "red"});
@@ -39,36 +37,26 @@ function pinThatAddress(address) {
     }));
 }
 
-function createPopUp(marker, address, data) {
-    console.log(data);
-    const popUp = new mapboxgl.Popup();
-    popUp.setHTML(
-        '<h3 class="text-capitalize mt-0 text-center">' + address + '</h3>' +
-        '<div class="card"></div>' +
-        '<div class="card-body m-0 p-0">' +
-        '<h5>'+ Math.round(data.list[0].main.temp) + '°F</h5>' +
-        '<h6 class="card-subtitle mb-2 text-muted">' + data.list[0].weather[0].description + '</h6>' +
-        '<p class="card-text m-0">H: ' + Math.round(data.list[0].main.temp_max) + '°F</p>' +
-        '<p class="card-text m-0">L:  ' + Math.round(data.list[0].main.temp_min) + '°F</p>' +
-        '</div></div>'
-    );
-    marker.setPopup(popUp);
-}
 
 function getWeatherData(marker, address, result) {
     $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: NEW_OPENWEATHER_API_KEY,
-        lat:    result[1],
-        lon:   result[0],
-        units: "imperial"
-    }).done(function(data) {
+        lat: result[1],
+        lon: result[0], units: "imperial"
+    }).done(function (data) {
         createPopUp(marker, address, data);
         weeklyForecast(marker, address, data);
     });
 }
 
+function createPopUp(marker, address, data) {
+    const popUp = new mapboxgl.Popup();
+    popUp.setHTML('<h3 class="text-capitalize mt-0 text-center">' + address + '</h3>' + '<div class="card"></div>' + '<div class="card-body m-0 p-0">' + '<h5>' + Math.round(data.list[0].main.temp) + '°F</h5>' + '<h6 class="card-subtitle mb-2 text-muted">' + data.list[0].weather[0].description + '</h6>' + '<p class="card-text m-0">H: ' + Math.round(data.list[0].main.temp_max) + '°F</p>' + '<p class="card-text m-0">L:  ' + Math.round(data.list[0].main.temp_min) + '°F</p>' + '</div></div>');
+    marker.setPopup(popUp);
+}
 
-$("#hide-btn").click(function() {
+
+$("#hide-btn").click(function () {
     markers.forEach((marker) => {
         marker.remove();
     });
@@ -76,7 +64,7 @@ $("#hide-btn").click(function() {
     $("#unhide-btn").toggleClass("disabled");
 });
 
-$("#unhide-btn").click(function() {
+$("#unhide-btn").click(function () {
     markers.forEach((marker) => {
         marker.addTo(map);
     });
@@ -86,22 +74,15 @@ $("#unhide-btn").click(function() {
 
 function weeklyForecast(marker, address, data) {
     let index = 0;
-    let dayIndex = 1;
     $(".weekly-forecast").html("");
     for (let i = 0; i < 5; i++) {
-        $(".weekly-forecast").append(
-            '<div class="card weather-cards">' +
-            '<img src="http://openweathermap.org/img/w/' + data.list[index].weather[0].icon + '.png" class="card-img-top">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title"> Day ' + dayIndex + '</h5>' +
-            '<p class="card-text">' + Math.round(data.list[index].main.temp) + '°F</p></div>' +
-            '<ul>' +
-            '<li class="list-group-item">Humidity: ' + Math.round(data.list[index].main.humidity) + '</li>' +
-            '<li class="list-group-item">Wind: ' + (data.list[index].wind.speed).toFixed(1) + '</li>' +
-            '<li class="list-group-item">Pressure: ' + Math.round(data.list[index].main.pressure) + '</li>' +
-            '</ul></div>'
-        );
-        dayIndex++;
+        var date = new Date(data.list[index].dt_txt);
+        if (i === 0) {
+            date = "Today";
+        } else {
+            date = date.toDateString().substring(0, 3);
+        }
+        $(".weekly-forecast").append('<div class="card w-100 weather-cards">' + '<div class="d-flex flex-column justify-content-center card-body">' + '<div class="d-flex ">' + '<h5 class="card-title">' + date + '</h5>' + '<div>' + '<img src="http://openweathermap.org/img/wn/' + data.list[index].weather[0].icon + '@2x.png" class="card-img-top">' + '</div>' + '<p class="card-text text-center">' + Math.round(data.list[index].main.temp) + '°F</p>' + '</div>' + '</div>' + '<ul class="m-0 px-5 pt-2 pb-4">' + '<li class="list-group-item">Humidity: ' + Math.round(data.list[index].main.humidity) + '</li>' + '<li class="list-group-item">Wind: ' + (data.list[index].wind.speed).toFixed(1) + '</li>' + '<li class="list-group-item">Pressure: ' + Math.round(data.list[index].main.pressure) + '</li>' + '</ul>' + '</div>');
         index += 8;
     }
 }
